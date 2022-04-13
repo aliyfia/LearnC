c7ebc6c72e4307a11ec65a4b61fd7a78c397aaaa
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-13 09:10:44 +0800

    Chapter2

---
+++ First Program/Hello Wrold.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hello World" << std::endl; //std::cout<<"print something"
+    return 0;
+}
---
+++ First Program/Print.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Number 1" << std::endl; //std::cout<<"print something"
+    std::cout<<"Number 2" << std::endl; //std::endl; print in newline
+    return 0;
+}
