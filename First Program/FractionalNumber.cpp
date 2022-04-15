d286e008afc3dbf96cc86e3b263242a0ccf0a098
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-15 09:38:42 +0800

    Fractional Number

--- First Program/FractionalNumber.cpp
+++ First Program/FractionalNumber.cpp
@@ -7,8 +7,13 @@ int main(){
     double number2 {1.12345678901234567890};
     long double number3 {1.12345678901234567890L};
 
-    std::cout << "size of number 1 (Float) = " << sizeof(number1) <<std::endl;
-    std::cout << "size of number 2 (double) = " << sizeof(number2) <<std::endl;
-    std::cout << "size of number 3 (long double) = "<< sizeof(number3) <<std::endl;
+    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << " // "<< number1 << std::endl;
+    std::cout << "size of number 2 (double)         = " << sizeof(number2) << " // " << number2 << std::endl;
+    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << " // "<< number3 << std::endl;
+
+    std::cout <<  std::setprecision(20);
+    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << " // "<< number1 << std::endl;
+    std::cout << "size of number 2 (double)         = " << sizeof(number2) << " // " << number2 << std::endl;
+    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << " // "<< number3 << std::endl;
 
 }
