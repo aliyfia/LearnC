27717098a4ef2ae057ae380cb85459e989c06364
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-15 11:14:24 +0800

    Character

---
+++ First Program/Character.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    char var1{'H'};
+    char var2{'a'};
+    char var3{'l'};
+    char var4{'l'};
+    char var5{'o'};
+    char var6{' '};
+
+    char var7{'W'};
+    char var8{'o'};
+    char var9{'r'};
+    char var10{'l'};
+    char var11{'d'};
+
+    std::cout << var1 << std::endl;
+    std::cout << var2 << std::endl;
+    std::cout << var3 << std::endl;
+    std::cout << var4 << std::endl;
+    std::cout << var5 << std::endl;
+    std::cout << var6 << std::endl;
+    std::cout << var7 << std::endl;
+    std::cout << var8 << std::endl;
+    std::cout << var9 << std::endl;
+    std::cout << var10 << std::endl;
+    std::cout << var11 << std::endl;
+    std::cout << sizeof(var1) << std::endl;
+}
