24fe8ed16c24ca1b9723c3405ba25da43fe2f23e
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-15 09:59:06 +0800

    Fractional Number

--- First Program/FractionalNumber.cpp
+++ First Program/FractionalNumber.cpp
@@ -7,13 +7,19 @@ int main(){
     double number2 {1.12345678901234567890};
     long double number3 {1.12345678901234567890L};
 
-    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << " // "<< number1 << std::endl;
-    std::cout << "size of number 2 (double)         = " << sizeof(number2) << " // " << number2 << std::endl;
-    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << " // "<< number3 << std::endl;
+    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << "    // "<< number1 << std::endl;
+    std::cout << "size of number 2 (double)         = " << sizeof(number2) << "     // " << number2 << std::endl;
+    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << "      // "<< number3 << std::endl;
 
     std::cout <<  std::setprecision(20);
-    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << " // "<< number1 << std::endl;
-    std::cout << "size of number 2 (double)         = " << sizeof(number2) << " // " << number2 << std::endl;
-    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << " // "<< number3 << std::endl;
+    std::cout << "size of number 1 (Float)          = " << sizeof(number1)  << "    // "<< number1 << std::endl; // only reach 7 digit 
+    std::cout << "size of number 2 (double)         = " << sizeof(number2) << "     // " << number2 << std::endl; // only reach 15 digit
+    std::cout << "size of number 3 (long double)    = "<< sizeof(number3) << "      // "<< number3 << std::endl; // can be more than 15+++
+
+
+    std::cout << "______________________________________"<<std::endl;
+    double number4 {1.2332323e2};
+    std::cout <<"Number 4 is = " << number4 <<std::endl; 
+
 
 }
