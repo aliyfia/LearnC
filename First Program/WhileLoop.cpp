c9dc71e44cdb7fe70e73f2f7c832f999593776f9
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-18 14:48:22 +0800

    While Loop

---
+++ First Program/WhileLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int loop;
+    cout << "Input Your Loop ?\n";
+    cin >> loop;
+    int i = 0;
+    while ( i < loop)
+    {
+        cout << "Looping " << i++ << "\n";
+        /* code */
+    }
+    
+}
