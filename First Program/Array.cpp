8b4f99618c5cd72ea9953900e12ea7bdf05d51ab
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-18 15:43:55 +0800

    Array

---
+++ First Program/Array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    string Nama [3] = {"Afif", "Aliyfia", "Alfian"};
+
+    cout<< Nama[0] << endl;
+    cout<< Nama[1] << endl;
+    cout<< Nama[2] << endl;
+    int LenghtOfArray =sizeof(Nama) / sizeof(string);
+    cout<< LenghtOfArray;
+
+
+   
+
+}
