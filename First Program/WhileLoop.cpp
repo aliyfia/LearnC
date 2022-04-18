0b0661e608e80415d04ed75bcfc2f8f9c03866cf
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-18 15:14:08 +0800

    while do loop

--- First Program/WhileLoop.cpp
+++ First Program/WhileLoop.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
     int loop;
     cout << "Input Your Loop ?\n";
     cin >> loop;
     int i = 0;
-    while ( i < loop)
+    while (i < loop)
     {
         cout << "Looping " << i++ << "\n";
         /* code */
     }
+
+    cout << "_____________________________________\n";
+    i = 0;
+    do
+    {
+        cout << "do Looping " << i++ << "\n";
+    } while (i < loop);
     
 }
