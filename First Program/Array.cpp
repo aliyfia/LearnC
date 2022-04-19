b4cea378b83f221c66900dbab0b59cfdfb6fdc40
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-19 08:35:16 +0800

    Array

--- First Program/Array.cpp
+++ First Program/Array.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-int main () {
+int main()
+{
 
-    string Nama [3] = {"Afif", "Aliyfia", "Alfian"};
+    string Nama[3] = {"Afif", "Aliyfia", "Alfian"};
 
-    cout<< Nama[0] << endl;
-    cout<< Nama[1] << endl;
-    cout<< Nama[2] << endl;
-    int LenghtOfArray =sizeof(Nama) / sizeof(string);
-    cout<< LenghtOfArray;
+    cout << Nama[0] << endl;
+    cout << Nama[1] << endl;
+    cout << Nama[2] << endl;
+    int LenghtOfArray = sizeof(Nama) / sizeof(string);
+    cout << LenghtOfArray <<"\n";
 
-
-   
-
+    cout << "Input Array  0 : \n";
+    cin >> Nama[0];
+    cout << Nama[0] << endl;
+    cout << Nama[1] << endl;
+    cout << Nama[2] << endl;
 }
