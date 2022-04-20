3633d1a5cc04e52d8ef01a75ddb98490d25d4f5c
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-20 11:37:08 +0800

    Reference

---
+++ First Program/Reference.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int int_data{20};
+    double double_data{30};
+
+    // referance
+    int &ref_int_data{int_data};
+    double &ref_double_data{double_data};
+
+    cout << "int data           = " << int_data << endl;
+    cout << "&int data          = " << &int_data << endl;
+    cout << "double data        = " << double_data << endl;
+    cout << "&double data       = " << &double_data << endl;
+
+    cout << "_________________________________\n";
+    cout << "ref int data       = " << ref_int_data << endl;
+    cout << "&Rref int data     = " << &ref_int_data << endl;
+    cout << "ref double data    = " << ref_double_data << endl;
+    cout << "&ref double data   = " << &ref_double_data << endl;
+
+    return 0;
+}
