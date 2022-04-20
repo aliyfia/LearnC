c2c5b27f26fc6771d676dc1f291c8476793dff22
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-20 11:46:23 +0800

    Reference

--- First Program/Reference.cpp
+++ First Program/Reference.cpp
@@ -22,5 +22,21 @@ int main()
     cout << "ref double data    = " << ref_double_data << endl;
     cout << "&ref double data   = " << &ref_double_data << endl;
 
+    cout << endl;
+
+    int_data = 123;
+    double_data = 123.123;
+
+    cout << "int data           = " << int_data << endl;
+    cout << "&int data          = " << &int_data << endl;
+    cout << "double data        = " << double_data << endl;
+    cout << "&double data       = " << &double_data << endl;
+
+    cout << "_________________________________\n";
+    cout << "ref int data       = " << ref_int_data << endl;
+    cout << "&Rref int data     = " << &ref_int_data << endl;
+    cout << "ref double data    = " << ref_double_data << endl;
+    cout << "&ref double data   = " << &ref_double_data << endl;
+
     return 0;
 }
