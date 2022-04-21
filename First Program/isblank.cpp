7a56cf3eebe1d64cb3afee78ec99a7d4a0107452
Aliyfia <43579798+aliyfia@users.noreply.github.com>
2022-04-21 10:38:39 +0800

    find char

--- First Program/isblank.cpp
+++ First Program/isblank.cpp
@@ -6,6 +6,8 @@ int main()
     cout << "Testing blank Character " << endl;
 
     int BlankCharacter{};
+    int UppercaseCharacter{};
+    int LowercaseCharacter{};
     char Message[]{"Lets try count blank character. Guess how many character there is gonna count"};
     cout << Message << endl;
     for (size_t i = 0; i < size(Message); i++)
@@ -13,8 +15,18 @@ int main()
         if (isblank(Message[i]))
         {
             cout << "Hi I found blank character = " << i << endl;
-            BlankCharacter++;
+            ++BlankCharacter;
+        }
+        if ( isupper(Message[i])) {
+            cout << "Hi i found Upper Case Character = " << i << endl;
+            UppercaseCharacter++;
+        }
+        if ( islower(Message[i])) {
+            cout << "Hi i found lower Case Character = "<< i << endl;
+            LowercaseCharacter++;
         }
     }
     cout << "Total blank character = " << BlankCharacter << endl;
+    cout << "Total Upper case character = " << UppercaseCharacter << endl;
+    cout << "Total Lower case character = " << LowercaseCharacter << endl;
 }
